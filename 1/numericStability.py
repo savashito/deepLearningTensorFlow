ab6280ca04030141e09c45868a71ebdd39dc40c7
Rodrigo Savage <rodrigosavage@gmail.com>
2016-08-23 10:59:07 -0700

    DNN

---
+++ 1/numericStability.py
@@ -0,0 +1,4 @@
+a = 1000000000
+
+for i in xrange(a):
+	
--- 1/regresion.py
+++ 1/regresion.py
@@ -23,7 +23,7 @@ y = tf.nn.softmax(tf.matmul(x,W) + b)
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-for i in range(1000):
+for i in range(20000):
 	batch = mnist.train.next_batch(50)
 	train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
